34cab9f30a70b191e2b80ee24048624d08472685
Guilherme Hiago de Oliveira <gui.hiago.oliveira@gmail.com>
2022-02-24 18:40:41 -0300

    Inserindo Eventos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,4 +7,8 @@
 <h1>Algum título</h1>
 <img src="/img/banner.jpg" alt="Banner">
 
+@foreach($events as $event)
+    <p>{{ $event->title }} -- {{ $event->description }}</p>
+@endforeach
+
 @endsection
