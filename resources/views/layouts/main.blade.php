80d632bc0d03e2c062e2df15154ea6538a5084c5
Guilherme Hiago de Oliveira <gui.hiago.oliveira@gmail.com>
2022-02-25 16:05:09 -0300

    Instalando JetStream para autenticação de login

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -33,13 +33,29 @@
                     <li class="nav-item">
                         <a href="/events/create" class="nav-link">Criar Eventos</a>
                     </li>
+                    @auth
                     <li class="nav-item">
-                        <a href="/" class="nav-link">Entrar</a>
+                        <a href="/dashboard" class="nav-link">Meus Eventos</a>
                     </li>
                     <li class="nav-item">
-                        <a href="/" class="nav-link">Cadastrar</a>
+                        <form action="/logout" method="POST">
+                            @csrf
+                            <a href="/logout" class="nav-link" onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+                                Sair
+                            </a>
+                        </form>
+                    </li>
+                    @endauth
+                    @guest
+                    <li class="nav-item">
+                        <a href="/login" class="nav-link">Entrar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/register" class="nav-link">Cadastrar</a>
                     </li>
                     <li class="nav-item"></li>
+                    @endguest
                 </ul>
             </div>
         </nav>
@@ -48,7 +64,7 @@
         <div class="container-fluid">
             <div class="row">
                 @if(session('msg'))
-                    <p class="msg">{{ session('msg') }}</p>
+                <p class="msg">{{ session('msg') }}</p>
                 @endif
                 @yield('content')
             </div>
